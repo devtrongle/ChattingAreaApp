d12bd6cc626332f5b3e42f34cf5177cfea9de8c9
TrongLE <31721023+devtrongle@users.noreply.github.com>
2022-06-04 23:17:27 +0700

    update

--- app/src/main/java/com/example/chattingarea/Constant.java
+++ app/src/main/java/com/example/chattingarea/Constant.java
@@ -7,15 +7,21 @@ public final class Constant {
     public static final String GROUP_REF = "group";
     public static final String GROUP_Chat_REF = "groupChat";
 
+    public static final String TEXT = "text";
+    public static final String IMAGE = "image";
+    public static final String VIDEO = "video";
+
     public static final class StatusContacts {
         public static final String FRIEND = "friend";
         public static final String DENY = "deny";
         public static final String REQUEST = "request";
     }
 
+
     public enum StatusRequest{
         SUCCESS,
         NO_DATA,
         FAIL,
+        EXIST,
     }
 }
